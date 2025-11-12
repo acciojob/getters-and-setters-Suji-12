a7671b680dedf076eac86cfc2350f464aa1b7436
Suji-12 <23csec23sujithas@gmail.com>
2025-11-12 23:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 class Person {
   constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
   }
   get name() {
-    return this.name;
+    return this._name;
   }
   get age() {
-    return this.age;
+    return this._age;
   }
   set age(newAge) {
-    this.age = newAge;
+    this._age = newAge;
   }	
 }
 
